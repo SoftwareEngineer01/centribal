2bee31ef3190f19819dab2e5acdaa8d2fd6ad448
SoftwareEngineer01 <sistemsistemaweb@gmail.com>
2022-01-14 08:26:23 -0500

    Se crean los modelos y migraciones

--- database/migrations/2022_01_14_131037_create_catalogs_table.php
+++ database/migrations/2022_01_14_131037_create_catalogs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCustomersTable extends Migration
+class CreateCatalogsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,9 @@ class CreateCustomersTable extends Migration
      */
     public function up()
     {
-        Schema::create('customers', function (Blueprint $table) {
+        Schema::create('catalogs', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('mobile_phone');
-            $table->string('email')->unique();
-            $table->string('direction');
-            $table->string('address');
-            $table->string('city');
-            $table->string('country');
             $table->timestamps();
         });
     }
@@ -33,6 +27,6 @@ class CreateCustomersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('customers');
+        Schema::dropIfExists('catalogs');
     }
 }
---
+++ database/migrations/2022_01_14_131709_create_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('price');
+            $table->timestamp('date_order');
+            $table->timestamp('deliver_order');
+            $table->foreignId('customer_id')->constrained('customers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
